5ae2fa077842e3afff9b44168e9bb05e0762a331
Chirayu Sharma <thechirayu02@gmail.com>
2022-09-24 21:56:06 +0530

    fix: change base url

--- lib/locator.dart
+++ lib/locator.dart
@@ -8,7 +8,7 @@ import 'package:get_it/get_it.dart';
 
 void setup() {
   GetIt.I.registerLazySingleton<MyDio>(
-      () => MyDio(baseUrl: "https://crypticstaging.acmvit.in/"));
+      () => MyDio(baseUrl: "http://20.193.148.31"));
   GetIt.I.registerLazySingleton<ApiService>(() => ApiService());
 
   GetIt.I.registerSingleton<GAuthService>(GAuthService());
